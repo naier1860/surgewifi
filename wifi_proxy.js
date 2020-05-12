9ce2bf89efe955feaa5bb9f83d193731a360c9e3
naier1860 <mymail1860@126.com>
2020-05-12 08:59:47 +0800

    Update wifi_proxy.js

--- wifi_proxy.js
+++ wifi_proxy.js
@@ -1,12 +1,20 @@
 var wifiname = $network.wifi.ssid;
 var proxywifi = ["ASUS-CK","ASUS-CK-5G"];
-if (wifiname == proxywifi){
-	$surge.setOutboundMode("direct");
-	//$notification.post("Meeta_Remind","您目前处于WIFI-Proxy","Surge已自动变为直连模式");
+for (var i = 0; i < proxywifi.length; i++) {
+	if (wifiname==proxywifi[i]){
+		$surge.setOutboundMode("direct");
+		
+		setTimeout(function(){$notification.post("Meeta_Remind","您目前处于WIFI-Proxy"+"SSID:"+wifiname,"Surge已自动变为直连模式");}, 3000);
+		break;
+		
+	};
+	if (i==proxywifi.length-1){
+		$surge.setOutboundMode("rule");
+		
+		setTimeout(function(){$notification.post("Meeta_Remind","Surge已自动变为规则模式","");}, 3000);
 	
-}
-else{
-	$surge.setOutboundMode("rule");
-	//$notification.post("Meeta_Remind","Surge已自动变为规则模式","");
-}
+	}
+	
+	
+};
 $done();
